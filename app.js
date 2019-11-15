05258b31a4bda483c51ed83268c4e0251022f953
Cestevez21 <00120218@uca.edu.sv>
2019-11-15 11:20:33 -0600

    Agregando conexion a la bd

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var database = require('./Database/elephantsql');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -39,4 +39,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+database.conn();
+module.exports = app; 
