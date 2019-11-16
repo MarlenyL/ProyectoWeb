cdf2a5d4c5e3b7604a41235fa6acff3cf798a96a
Tu Nombre <you@example.com>
2019-11-15 18:20:14 -0600

    Solucionando problema de merge

--- routes/transacciones.js
+++ routes/transacciones.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SET' });
+  res.render('transacciones');
 });
 
 module.exports = router;
