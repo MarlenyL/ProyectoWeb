c60e9e2479a9f4d2f086d93adddae7666d4b1358
Gerardo Chinchilla <00049518@uca.edu.sv>
2019-11-16 10:56:17 -0600

    add: ruta transacciones, fix: css

--- routes/transaccionesE.js
+++ routes/transaccionesE.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('transacciones');
+  res.render('transaccionesE',  { title: 'SET' });
 });
 
 module.exports = router;
