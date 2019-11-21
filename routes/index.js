7a32342fef03352332377e2be374ebec9ecb9b0c
MarlenyL <00153118@uca.edu.sv>
2019-11-20 22:30:49 -0600

    Arreglando problemas del merge

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
-var path = require('path');
-var database = require('../Database/elephantsql');
-var app = express();
-var pg = require('pg')
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'STE' });
-});
-
-var conString = "postgres://bsotqenl:SIi5Keq54evR9YTlT-3uQLL57vg0DDGA@salt.db.elephantsql.com:5432/bsotqenl" //Can be found in the Details page
-var client = new pg.Client(conString);
-
 var StudentManager = require('../controllers/StudentManager');
 
 
