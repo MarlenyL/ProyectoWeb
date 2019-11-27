5f156a51fae9a0b83f2b356f65d635a52bc84dda
Gerardo Chinchilla <00049518@uca.edu.sv>
2019-11-27 15:30:31 -0600

    Uniendo  ramas

--- public/assets/JS/lateral.js
+++ public/assets/JS/lateral.js
@@ -4,7 +4,7 @@ function myFunction() {
  }
 
   window.onload = function() {
-    var tbody = document.querySelector("#table_body");
+    var lateral = document.querySelector(".fa-bars");
     console.log("Abre esta madre");
         
     addRow("Compra: Libreria Uca", "15/nov", 12563, -2.25, 14.00, tbody);
