00f9465e4370af643eef157c538f13278c85a482
FerVH26 <00179118@uca.edu.sv>
2019-11-27 15:19:23 -0600

    Fix

--- public/assets/JS/ayuda.js
+++ public/assets/JS/ayuda.js
@@ -1,7 +1,9 @@
 window.onload = function(){
     var ayudabutton = document.querySelector("#ayuda");
     var divayuda= document.querySelector(".help");
+    
     ayudabutton.addEventListener("click",()=>{
         divayuda.classList.toggle("mostrar");
     })
+
 }
