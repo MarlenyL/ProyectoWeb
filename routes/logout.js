bbef23aca93984f75eb865e4a26a292044720e66
Cestevez21 <00120218@uca.edu.sv>
2019-11-27 23:44:41 -0600

    Fix: Error de conexion

--- routes/logout.js
+++ routes/logout.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var passport   = require('passport');
 var authController = require('../controllers/authcontroller');
 
 router.get('/', authController.logout);
