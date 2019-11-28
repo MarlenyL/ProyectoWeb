a08e944051b04c8e840d60afb45c9475d54a46bf
Gerardo Chinchilla <00049518@uca.edu.sv>
2019-11-27 22:55:48 -0600

    fix: detalles

--- public/assets/JS/lateral.js
+++ public/assets/JS/lateral.js
@@ -1,14 +1,9 @@
-function myFunction() {
-    var element = document.getElementById("myDIV");
-    element.classList.toggle("mystyle");
- }
-
   window.onload = function() {
     var lateral = document.querySelector(".fa-bars");
-    console.log("Abre esta madre");
-        
-    addRow("Compra: Libreria Uca", "15/nov", 12563, -2.25, 14.00, tbody);
+    var addmenu = document.querySelector("#lateral");
 
+    lateral.addEventListener("click",()=>{
+      addmenu.classList.toggle("menu");
 
-      
-  };
+    })
+  }
