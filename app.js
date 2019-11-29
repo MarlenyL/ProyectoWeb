025c81eb2fa549615b90ad3eacb6aaa53c4ac827
Gerardo Chinchilla <00049518@uca.edu.sv>
2019-11-28 23:56:40 -0600

    añadiendo rutas

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var vendedorRouter = require('./routes/vendedor');
 var session = require('express-session');
 var logoutRouter = require('./routes/logout');
 var datospRouter = require('./routes/datospersonales')
-// var recargaRouter = require('./routes/recarga');
+var recargaRouter = require('./routes/recarga');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -84,7 +84,7 @@ app.use('/logout',logoutRouter);
 app.use('/estadisticasE',estadisticasRouter);
 app.use('/vendedor',vendedorRouter);
 app.use('/datosP',datospRouter);
-// app.use('/recarga',recargaRouter)
+app.use('/recarga',recargaRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/vendedor.js
+++ routes/vendedor.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var StudentManager = require('../controllers/StudentManager');
+var passport = require('passport');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('vendedor', );
+router.get('/',isLoggedIn,function(req, res, next) {
+    StudentManager.saldo(req.user.id, function (mod) {
+        var data = {saldo:mod, user:req.user};
+        res.render('vendedor',data)
+    });  
 });
 
+function isLoggedIn(req, res, next) {
+ 
+  if (req.isAuthenticated()){
+      
+      return next();
+  }
+  res.redirect('/signin');
+
+}
 module.exports = router;
